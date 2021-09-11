8e78da497d7c852b7055cbaa6aea4d7a97faebec
saurabhs <saurabhpshirsath@gmail.com>
2021-09-11 06:29:05 +0530

    [saurabh] Add: Checked Employee Is Present or Absent

---
+++ Employee.js
@@ -0,0 +1,12 @@
+//UC1-Check Attendance of Employee
+const IS_ABSENT =0;
+let empCheck=Math.floor(Math.random()*10)%2;
+if(empCheck == IS_ABSENT)
+{
+    console.log("Employee Is Absent");
+    return;
+}
+else
+{
+    console.log("Employee Is present");
+}
